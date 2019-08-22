ea5b0f3569f2e747e00ed4997fe5d9059dfde899
JacobForsman <43602729+JacobForsman@users.noreply.github.com>
2019-08-22 09:01:37 +0300

    Nä fö vafö sku e ga ti push

---
+++ script.js
@@ -0,0 +1,33 @@
+let numberInput = document.getElementById('number');
+let submitButton = document.getElementById('button');
+let resultElement = document.getElementById('result');
+
+submitButton.addEventListener('click', onButtonClicked);
+
+let numberToGuess = Math.round(Math.random() * 20);
+
+function onButtonClicked(event) {
+    guess(numberInput.value, numberToGuess);
+    event.preventDefault();
+}
+
+function guess(guessedValue, numberToGuess) {
+    let guessedNumber = parseInt(guessedValue);
+    if (isNaN(guessedNumber)) {
+        resultElement.innerHTML = 'Your guess must be a number!';
+        return;
+    } else if (guessedNumber < 0 || guessedNumber > 20) {
+        resultElement.innerHTML = 'The guessed number must be between 0 and 20';
+        return;
+    }
+
+    if (guessedNumber < numberToGuess) {
+        resultElement.innerHTML = 'Your guess is too low';
+        return;
+    } else if (guessedNumber > numberToGuess) {
+        resultElement.innerHTML = 'Your guess is too high';
+        return;
+    }
+
+    resultElement.innerHTML = 'You guessed the correct number!';
+}
